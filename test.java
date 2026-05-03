3732e21253464955988b2038af62e7708c33b131
Shivam Jain <jainshivam2427@gmail.com>
2026-05-04 00:16:53 +0530

    Refactor test.java with additional methods

--- test.java
+++ test.java
@@ -15,6 +15,7 @@ public class test {
         System.out.println(a);
         System.out.println(b);
     }
+    
 
     public static void fib(int n) {
         int a = 0;
@@ -322,6 +323,94 @@ public class test {
     }
     return max;
 }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+                if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int low = 0, high = m;
+
+        while (low <= high) {
+            int cut1 = (low + high) / 2;
+            int cut2 = (m + n + 1) / 2 - cut1;
+
+            int left1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int left2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            int right1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
+            int right2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (left1 <= right2 && left2 <= right1) {
+                if ((m + n) % 2 == 1) {
+                    return Math.max(left1, left2);
+                }
+                return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
+            }
+            else if (left1 > right2) {
+                high = cut1 - 1;
+            }
+            else {
+                low = cut1 + 1;
+            }
+        }
+
+        return 0.0;
+    }
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (s == null || s.length() == 0 || words.length == 0) {
+            return result;
+        }
+
+        int wordLen = words[0].length();
+        int totalWords = words.length;
+        int totalLen = wordLen * totalWords;
+
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String w : words) {
+            wordMap.put(w, wordMap.getOrDefault(w, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLen; i++) {
+            int left = i;
+            int count = 0;
+            Map<String, Integer> windowMap = new HashMap<>();
+
+            for (int j = i; j + wordLen <= s.length(); j += wordLen) {
+                String word = s.substring(j, j + wordLen);
+
+                if (wordMap.containsKey(word)) {
+                    windowMap.put(word, windowMap.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (windowMap.get(word) > wordMap.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        windowMap.put(leftWord, windowMap.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == totalWords) {
+                        result.add(left);
+                    }
+
+                } else {
+                    windowMap.clear();
+                    count = 0;
+                    left = j + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+        
+
 
     public static void main(String[] args) {
 test.swap(2,4);
